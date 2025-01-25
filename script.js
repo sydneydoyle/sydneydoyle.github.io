3997e955cefbd591a09a1bde8533235ce4f5e66c
Sydney Doyle <106398877+sydneydoyle@users.noreply.github.com>
2025-01-25 16:39:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function checkPassword(event) {
     alert("Incorrect password. Contact skdoyle@ymail.com to access.");
   }
 }
+
+var accItem = document.getElementsByClassName('accordionItem');
+var accHD = document.getElementsByClassName('accordionItemHeading');
+for (i = 0; i < accHD.length; i++) {
+    accHD[i].addEventListener('click', toggleItem, false);
+}
+function toggleItem() {
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'accordionItem close';
+    }
+    if (itemClass == 'accordionItem close') {
+        this.parentNode.className = 'accordionItem open';
+    }
+};
+$(document).ready(function(){
+$('#portests').on('click',function(){	
+    $('.test').toggle();
+});
+$('#portests1').on('click',function(){	
+    $('.tests').toggle();
+});
+});
